d2dbfdcc57411054b9271c74cd3bee97d8eb14c3
IukeByte1 <57533013+IukeByte1@users.noreply.github.com>
2019-11-08 09:14:16 -0500

    Add files via upload

---
+++ Canvas-Exporter.bat
@@ -0,0 +1,24 @@
+@echo off
+:start
+echo Select type of export:
+echo =============
+echo -
+echo 1) Discussion Export
+echo 2) Exit
+set /p exporter= 
+if "%exporter%"=="1" goto discussion
+if "%exporter%"=="2" goto exit
+:discussion
+cls
+echo Canvas Discussion Exporter By Luke F. (C 2019)
+echo Note: Remove the "while(1);" header and you will be able to work with the output in JSON.
+echo Note: The * marks the part of the URL that you are looking for.
+set /p school=Enter School Name (*SchoolName*.instructure.com): 
+set /p course=Enter Course ID (SchoolName.instructure.com/courses/*CourseID*): 
+set /p discussion=Enter Discussion ID (SchoolName.instructure.com/courses/CourseID/discussion_topics/*DiscussionID*): 
+start "" https://%school%.instructure.com/api/v1/courses/%course%/discussion_topics/%discussion%/view?include_new_entries=1
+echo Done! Right click and then press "Save As..." to download the .json.
+pause
+goto exit
+:exit
+exit
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# CanvasTools
-Canvas Tools
+# CanvasDiscussionExporter
+Uses Canvas API to grab discussion as .json data
+
+Thanks to [Markus Kreitzer](https://community.canvaslms.com/ideas/5356-download-discussion-board-posts#comment-70516) for the manual API grabbing method
+
+Thanks to [Daniel Puperi](https://github.com/dsp444/save_canvas_discussion) for the .json to .html python script included in this repo.
